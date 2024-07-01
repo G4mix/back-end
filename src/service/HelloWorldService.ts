2fe60efd6ba6d2ebfd2adf9c4f934d8b608a3221
Lucas-Christian <lucas.christian.programmer@gmail.com>
2024-06-30 22:20:48 -0300

    feat: s3 service

--- src/service/HelloWorldService.ts
+++ src/service/HelloWorldService.ts
@@ -1,12 +1,14 @@
 import { HelloWorldRepository } from '@repository'
 import { injectable, singleton } from 'tsyringe'
+import { S3Service } from '@service/S3Service'
 
 @injectable()
 @singleton()
 export class HelloWorldService {
-	constructor(private helloWorldRepository: HelloWorldRepository) {}
+	constructor(private helloWorldRepository: HelloWorldRepository, private s3Service: S3Service) {}
 
 	public async hello(): Promise<string> {
+		await this.s3Service.initializeBuckets()
 		return await this.helloWorldRepository.hello()
 	}
 }
