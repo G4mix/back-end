b09e5dedefb3c86b7e752a3a5ddbfbb82c22e426
Lucas Christian Freitas de Olivera <65835432+LordLuch@users.noreply.github.com>
2024-08-30 00:44:44 +0000

    feat: jwt middleware

---
+++ src/middlewares/security/jwt.ts
@@ -0,0 +1,33 @@
+import type { Response as ExResponse } from 'express'
+import { Claims, JwtManager } from '@utils'
+import { sendErrorMessage } from '.'
+import { container } from '@ioc'
+import { PrismaClient } from '@prisma/client'
+
+export const jwtMiddleware = async ({
+	res, token
+}: { res: ExResponse; token: string; }): Promise<
+	| void
+	| Claims
+	| ExResponse<unknown, Record<string, unknown>>
+> => {
+	let claims: Claims
+	try {
+		claims = JwtManager.decode(token)
+	} catch (err) {
+		return sendErrorMessage({ res })
+	}
+	if (!claims['verifiedEmail']) return sendErrorMessage({ res, message: 'EMAIL_NOT_VERIFIED'})
+		
+	const pg = container.resolve<PrismaClient>('PostgresqlClient')
+	const user = await pg.user.findUnique({
+		where: { id: claims['sub'] }
+	})
+	if (!user) return sendErrorMessage({ res, message: 'USER_NOT_FOUND' })
+
+	if (JwtManager.isTokenExpiringIn20Minutes(token) && !res.hasHeader('Authorization')) {
+		res.appendHeader('Authorization', `Bearer ${JwtManager.refreshToken(token)}`)
+	}
+
+	return claims
+}
