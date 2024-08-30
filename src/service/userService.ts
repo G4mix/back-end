beb49e5eeaec1ad9210c29cefac7708f90dd769b
Lucas Christian Freitas de Olivera <65835432+LordLuch@users.noreply.github.com>
2024-08-30 00:44:33 +0000

    feat: user update (short version)

---
+++ src/service/userService.ts
@@ -0,0 +1,17 @@
+import { injectable, singleton } from 'tsyringe'
+import { UserRepository } from '@repository'
+import { BCryptEncoder, JwtManager } from '@utils'
+
+@injectable()
+@singleton()
+export class UserService {
+	constructor(
+		private userRepository: UserRepository,
+	) {}
+
+	public async update(data: { id: string; username?: string; email?: string; password?: string; }) {
+		if (data.password) data.password = BCryptEncoder.encode(data.password)
+		const user = await this.userRepository.update(data)
+		return { token: JwtManager.generateToken({ sub: user.id, user }) }
+	}
+}
