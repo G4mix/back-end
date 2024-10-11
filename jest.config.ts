04eaacbde87ad271143297428c65468244a613f7
Lucas Christian <lucas.christian.programmer@gmail.com>
2024-10-11 18:14:34 -0300

    feat: production and preview deploy

--- jest.config.ts
+++ jest.config.ts
@@ -6,14 +6,23 @@ const jestConfig: JestConfigWithTsJest = {
 	testEnvironment: 'node',
 	moduleDirectories: ['node_modules', '<rootDir>/src'],
 	modulePaths: [compilerOptions.baseUrl],
-	moduleNameMapper: pathsToModuleNameMapper(compilerOptions.paths),
+	moduleNameMapper: pathsToModuleNameMapper(compilerOptions.paths, { prefix: '<rootDir>/' }),
 	testRegex: 'src/.*\\.(test|spec)\\.[jt]s$',
 	transform: {
 		'^.+\\.ts?$': 'ts-jest',
 	},
-	collectCoverageFrom: ['src/**/*.ts', '!src/index.ts', '!src/**/*.d.ts', '!**/node_modules/**', '!src/__tests__/**/*.ts', '!src/tsoa/**', '!src/constants/**'],
+	collectCoverageFrom: [
+		'src/**/*.ts',
+		'!src/index.ts',
+		'!src/**/*.d.ts',
+		'!**/node_modules/**',
+		'!src/__tests__/**/*.ts',
+		'!src/tsoa/**',
+		'!src/constants/**',
+	],
 	coverageDirectory: 'coverage',
 	coverageReporters: ['lcov', 'text-summary'],
 	setupFilesAfterEnv: ['reflect-metadata'],
 }
+
 export default jestConfig
