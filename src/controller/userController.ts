8ddc2e5fb645208df3e5e42ae393667c5960a201
Lucas Christian <110307926+Lucas-Christian@users.noreply.github.com>
2025-03-15 15:34:11 +0000

    fix: all ts errors

--- src/controller/userController.ts
+++ src/controller/userController.ts
@@ -35,8 +35,7 @@ export class UserController extends Controller {
 		}
 		const res = await this.userService.update(data)
 		if (typeof res === 'string') return ControllerUtils.handleResponse(res, this)
-		this.setHeader('Authorization', `Bearer ${res.token}`)
-		return res.user
+		return res
 	}
 
 	/**
