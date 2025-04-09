757fb13afc98cbd584af538cbb297920b98a9e39
Lucas Christian <lucas.christian.programmer@gmail.com>
2025-04-09 12:08:52 -0300

    fix: test format json

--- src/schemas/postSchema.ts
+++ src/schemas/postSchema.ts
@@ -2,47 +2,5 @@ import { z } from 'zod'
 
 export const postSchema = z.object({
 	title: z.undefined().or(z.string().regex(/^[^{}]{3,70}$/, 'INVALID_TITLE')),
-	content: z.undefined().or(z.string().regex(/^[^{}]{3,700}$/, 'INVALID_CONTENT')),
-	links: z
-		.union([
-			z.undefined(),
-			z
-				.string()
-				.transform((val) => {
-					try {
-						return JSON.parse(val)
-					} catch {
-						return []
-					}
-				})
-				.pipe(z.array(z.string().url()).max(5, 'TOO_MANY_LINKS')),
-		]),
-	tags: z
-		.union([
-			z.undefined(),
-			z
-				.string()
-				.transform((val) => {
-					try {
-						return JSON.parse(val)
-					} catch {
-						return []
-					}
-				})
-				.pipe(z.array(z.string()).max(10, 'TOO_MANY_TAGS')),
-		]),
-	images: z
-		.union([
-			z.undefined(),
-			z
-				.string()
-				.transform((val) => {
-					try {
-						return JSON.parse(val)
-					} catch {
-						return []
-					}
-				})
-				.pipe(z.array(z.string().url()).max(8, 'TOO_MANY_IMAGES')),
-		])
+	content: z.undefined().or(z.string().regex(/^[^{}]{3,700}$/, 'INVALID_CONTENT'))
 })
