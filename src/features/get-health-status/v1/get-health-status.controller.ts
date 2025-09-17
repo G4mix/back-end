628072773677b6e5c3cfc657ed565b1b37ebe9b3
Lucas-Christian <lucas.christian.programmer@gmail.com>
2025-09-17 20:47:06 -0300

    feat: remove protection from health endpoint
    Co-authored-by: gabrielOliv1 <gabrielOliv1@users.noreply.github.com>
    Co-authored-by: André Lucas S.S. <andrelucassouzasilva@users.noreply.github.com>

--- src/features/get-health-status/v1/get-health-status.controller.ts
+++ src/features/get-health-status/v1/get-health-status.controller.ts
@@ -1,6 +1,5 @@
 import { Controller, Get, Logger, Version } from '@nestjs/common';
 import { LogResponseTime } from 'src/shared/decorators/log-response-time.decorator';
-import { Protected } from 'src/shared/decorators/protected.decorator';
 
 @Controller('/')
 export class GetHealthStatusController {
@@ -8,7 +7,6 @@ export class GetHealthStatusController {
 
   @Get('/health')
   @Version('1')
-  @Protected()
   @LogResponseTime()
   getHealthStatus(): { status: string } {
     return { status: 'ok' };
